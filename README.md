34d07e25a58d41825062706026d461375bad9387
stargazer8tw <stargazer.taiwan@gmail.com>
2014-11-15 09:45:14 +0800

    update changes

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ pateko
 ======
 
 web dev
+
+
+---
+
+
+> **Reference:**
+
+> - pagedown: https://code.google.com/p/pagedown/
--- public/lib/pagedown/node-pagedown.js
+++ public/lib/pagedown/node-pagedown.js
@@ -1,6 +1,2 @@
-var imported = document.createElement('script');
-imported.src = 'vendor/require.js';
-document.head.appendChild(imported);
-
 exports.Converter = require("./Markdown.Converter").Converter;
 exports.getSanitizingConverter = require("./Markdown.Sanitizer").getSanitizingConverter;
